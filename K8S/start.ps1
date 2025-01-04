242cd7992709db272e5f671dbc5e6b3ee30d6b5d
Timur Makimov <megapers@gmail.com>
2025-01-03 23:58:17 -0800

    Fix: switched services from NodePort to ClusterIP

--- K8S/start.ps1
+++ K8S/start.ps1
@@ -1,7 +1,8 @@
 kubectl apply `
     -f rabbitmq.yaml `
     -f pubsub-rabbitmq.yaml `
+    -f platforms-np-srv.yaml `
     -f platforms-service.yaml `
     -f commands-service.yaml `
     -f commands-depl.yaml `
-    -f platforms-depl.yaml
+    -f platforms-depl.yaml 
